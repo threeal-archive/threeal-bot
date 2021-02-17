db955a8b3cc0c5b43ce3dd62576c699fe53b8d15
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-02-17 20:46:22 +0700

    bot echoing message

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
-const dotenv = require('dotenv');
+import { Client, Message } from 'discord.js';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
+const client: Client = new Client();
 
 client.once('ready', () => {
-  console.log('Bot ready!');
+  console.log(`Bot ${client.user.username} is ready!`);
+});
+
+client.on('message', (message: Message) => {
+  console.log(`Received ${message.cleanContent} from ${message.author.username}`);
+
+  if (message.author.id != client.user.id) {
+    message.channel.send(message.content);
+  }
 });
 
 console.log(`Login using token ${process.env.TOKEN}`);
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
+  .catch((err: Error) => {
+    console.error(`Login failed! ${err.message}`);
+  });
