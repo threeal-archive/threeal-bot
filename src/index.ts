8deb5dd86cf2bc9e839f820d2356d808c330868f
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-02-17 20:53:00 +0700

    add chalk for colored console output

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 import { Client, Message } from 'discord.js';
 import * as dotenv from 'dotenv';
+import * as chalk from 'chalk';
 
 dotenv.config();
 
 const client: Client = new Client();
 
 client.once('ready', () => {
-  console.log(`Bot ${client.user.username} is ready!`);
+  console.log(`Bot ${chalk.green(client.user.username)} is ready!`);
 });
 
 client.on('message', (message: Message) => {
-  console.log(`Received ${message.cleanContent} from ${message.author.username}`);
+  if (message.author.id !== client.user.id) {
+    console.log(`Received ${chalk.green(message.cleanContent)} from ${chalk.green(message.author.username)}`);
 
-  if (message.author.id != client.user.id) {
     message.channel.send(message.content);
   }
 });
 
-console.log(`Login using token ${process.env.TOKEN}`);
+console.log(`Login using token ${chalk.green(process.env.TOKEN)}`);
 client.login(process.env.TOKEN)
   .catch((err: Error) => {
-    console.error(`Login failed! ${err.message}`);
+    console.error(`Login failed! ${chalk.red(err.message)}`);
   });
