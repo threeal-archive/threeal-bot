8f4348af0fc601d1265e218ce5d22bc1b5692ca1
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-02-19 10:56:56 +0700

    add prettier and lint fix

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
-import { Client, Message } from 'discord.js';
-import * as dotenv from 'dotenv';
-import * as chalk from 'chalk';
+import { Client, Message } from "discord.js";
+import * as dotenv from "dotenv";
+import * as chalk from "chalk";
 
 dotenv.config();
 
 const client: Client = new Client();
 
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Bot ${chalk.green(client.user.username)} is ready!`);
 });
 
-client.on('message', (message: Message) => {
+client.on("message", (message: Message) => {
   if (message.author.id !== client.user.id) {
-    console.log(`Received ${chalk.green(message.cleanContent)} from ${chalk.green(message.author.username)}`);
+    console.log(
+      `Received ${chalk.green(message.cleanContent)}` +
+        `from ${chalk.green(message.author.username)}`
+    );
 
     message.channel.send(message.content);
   }
 });
 
 console.log(`Login using token ${chalk.green(process.env.TOKEN)}`);
-client.login(process.env.TOKEN)
-  .catch((err: Error) => {
-    console.error(`Login failed! ${chalk.red(err.message)}`);
-  });
+client.login(process.env.TOKEN).catch((err: Error) => {
+  console.error(`Login failed! ${chalk.red(err.message)}`);
+});
